0b25e86a5b8ccded8c743e0ecb5cac9210de9d21
Alexey Gennadievich <xela07ax@users.noreply.github.com>
2023-09-06 16:55:53 +0300

    Update listenArifmetic.go

--- listenArifmetic.go
+++ listenArifmetic.go
@@ -18,15 +18,15 @@ const MathematicSymbols = "+-"
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	log.Print("got / request")
 
-	ua := r.Header.Get("User-Access")
+	//ua := r.Header.Get("User-Access")
 	// API должно быть доступно только если в HTTP Header есть “User-Access”
 	//со значением “superuser”. В случае отказа в доступе, нужно вывести
 	//сообщение в консоли и отправить соответствующий ответ клиенту.
-	if ua != AccessHeader {
-		log.Printf("неверный заголовок входа User-Access:%s", ua)
-		http.Error(w, ErrHeader, http.StatusForbidden)
-		return
-	}
+	//if ua != AccessHeader {
+	//	log.Printf("неверный заголовок входа User-Access:%s", ua)
+	//	http.Error(w, ErrHeader, http.StatusForbidden)
+	//	return
+	//}
 	//только операции сложения и вычитания.
 	//Например, с Frontend-а к тебе приходит строка “2+2-3-5+1”.
 	bytes, err := io.ReadAll(r.Body)
@@ -34,6 +34,9 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 		log.Printf("не удается прочитать тело сообщения err:%s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
+	}
+	if len(bytes) == 0 {
+		bytes = []byte(r.URL.RawQuery)
 	}
 	// поскольку мы имеем дело только с цифрами и знаками, то можно итерироваться по байтам
 	var nums []int = make([]int, 1)
@@ -53,7 +56,8 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, textErr, http.StatusInternalServerError)
 				return
 			}
-			fullInt, err := strconv.Atoi(fmt.Sprintf("%d%d", nums[len(nums)-1], intVar))
+			//fullInt, err := strconv.Atoi(fmt.Sprintf("%d%d", nums[len(nums)-1], intVar)) //old
+			fullInt := nums[len(nums)-1]*10 + intVarm
 			nums[len(nums)-1] = fullInt
 		}
 	}
@@ -82,7 +86,8 @@ func getRoot(w http.ResponseWriter, r *http.Request) {
 func main() {
 	fmt.Println("")
 	http.HandleFunc("/", getRoot)
-
+	fmt.Println("http://localhost:3333/?2+2-3-5+1\n")
+	fmt.Println("http://localhost:3333/?852+7656-133\n")
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("server closed ok\n")
